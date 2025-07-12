2b7655eb8f0d5eb78fcdc9dcf85e603dd00df373
kyungtae.hwang <kyungtae.hwang@navercorp.com>
2025-07-12 14:37:24 +0900

    u

--- src/client/messenger/imessenger.js
+++ src/client/messenger/imessenger.js
@@ -1,8 +1,6 @@
 import { Container, Graphics, Text, TextStyle } from 'pixi.js';
 import global from '../global/index.js';
 
-export const TRANSPARENT_1PX_IMG = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAABmJLR0QA/wD/AP+gvaeTAAAACXBIWXMAAC4jAAAuIwF4pT92AAAAB3RJTUUH5QkWDxoxGJD3fwAAABl0RVh0Q29tbWVudABDcmVhdGVkIHdpdGggR0lNUFeBDhcAAAALSURBVAjXY2AAAgAABQAB4iYFmwAAAABJRU5ErkJggg==';
-
 const NAME_STYLE = new TextStyle({
   fontSize: 24,
   fontStyle: 'italic',
@@ -21,8 +19,7 @@ const name = new Text({ text: '', style: NAME_STYLE });
  */
 const getMessageStyle = (width) => new TextStyle({
   fontFamily: 'Arial',
-  fontSize: 24,
-  // fontSize: 18,
+  fontSize: 22,
   wordWrap: true,
   wordWrapWidth: width,
   fill: 0xffffff,
@@ -38,42 +35,31 @@ const show = ({ name: _name, message, portrait }) => {
   const appHeight = application.screen.height;
 
   bg.clear();
-  bg.rect(0, 0, appWidth, appHeight / 2 - 48);
+  bg.rect(0, 0, appWidth, Math.min(150, appHeight / 2 - 48));
   bg.fill({ color: 0x000000, alpha: 0.7 });
   bg.x = 0;
   bg.y = appHeight - bg.height;
   container.addChild(bg);
 
-  name.text = _name;
+  name.text = _name + ' ';
 
   if (portrait) {
-    const photoSize = Math.min(144, Math.min(appWidth, appHeight) / 2);
+    const photoSize = Math.min(144, Math.min(appWidth, appHeight) / 2 - 64);
     portrait.width = photoSize;
     portrait.height = photoSize;
-    portrait.x = 12;
-    portrait.y = bg.y + bg.height - photoSize - 12;
+    portrait.y = bg.y - photoSize;
     container.addChild(portrait);
-
-    name.x = portrait.x + portrait.width + 12;
-    name.y = bg.y + 6;
-    container.addChild(name);
-    token.style = getMessageStyle(bg.width - photoSize - 36);
-    token.x = portrait.x + portrait.width + 12;
-    token.y = name.y + name.height + 6;
-    container.addChild(token);
-  } else {
-    name.x = 12;
-    name.y = bg.y + 6;
-    container.addChild(name);
-
-    token.style = getMessageStyle(bg.width - 36);
-    token.x = 12;
-    token.y = name.y + name.height + 6;
-    container.addChild(token);
   }
+  name.x = 12;
+  name.y = bg.y + 6;
+  container.addChild(name);
 
-  /** @type {string[]} */
-  const textList = [].concat(message);
+  token.style = getMessageStyle(bg.width - 36);
+  token.x = 12;
+  token.y = name.y + name.height + 6;
+  container.addChild(token);
+
+  const textList = Array.isArray(message) ? message : [message];
   let listIdx = 0;
   let msgStartIdx = 0;
   let msgEndIdx = 0;
@@ -104,12 +90,17 @@ const show = ({ name: _name, message, portrait }) => {
   return new Promise((resolve) => {
     container.eventMode = 'static';
     showPartedMessage();
+    /**
+     * @param {import('pixi.js').FederatedPointerEvent} evt
+     */
     const handler = (evt) => {
       evt.stopPropagation();
       if (listIdx >= textList.length) {
         application.stage.removeChild(container);
         container.off('pointertap', handler);
-        container.removeChild(portrait);
+        if (portrait) {
+          container.removeChild(portrait);
+        }
         resolve(undefined);
       } else {
         showPartedMessage();
