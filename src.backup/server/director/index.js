6b4f4f1abdde1e4d772ea7bc05dbdcb63f3d9307
kyungtae.hwang <kyungtae.hwang@navercorp.com>
2025-12-20 07:43:38 +0900

    remove backup code

--- src.backup/server/director/index.js
+++
@@ -1,3 +0,0 @@
-export { CameraDirector } from './camera.js';
-export { EffectDirector } from './effect.js';
-export { StageDirector } from './stage.js';
--- src.backup/server/index.js
+++
@@ -1,5 +0,0 @@
-import { ShardForge } from './shard/forge.js';
-import { Shard } from './shard/index.js';
-import { ServerReceiver } from './receiver/index.js';
-
-export { ServerReceiver, ShardForge, Shard };
